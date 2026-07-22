c5cdc9e61ae15e194fb9a007d48a3851d7586822
zehdso <zehdsox@gmail.com>
2026-07-23 01:40:18 +0530

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,14 +2,16 @@ import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_ACTIONS === "true";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: isGithubPages ? "/OpenPharmaDB" : "",
-  assetPrefix: isGithubPages ? "/OpenPharmaDB/" : "",
-};
+const nextConfig: NextConfig = isGithubPages
+  ? {
+      output: "export",
+      trailingSlash: true,
+      images: {
+        unoptimized: true,
+      },
+      basePath: "/OpenPharmaDB",
+      assetPrefix: "/OpenPharmaDB/",
+    }
+  : {};
 
 export default nextConfig;
